e152f6196b186060d62e9614293992b0e3a53ca1
NTP09 <ntplefebvre@yahoo.com>
2024-02-23 13:21:43 -0500

    Version 1.2

--- public/blackjack.js
+++ public/blackjack.js
@@ -1,15 +1,73 @@
-function createCards() {
-    const deckElement = document.getElementById('deck');
-    const suits = ['C', 'D', 'H', 'S']; 
-    const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+var dealerSum = 0;
+var playerSum = 0;
 
-    suits.forEach(suit => {
-        values.forEach(value => {
-            const cardElement = document.createElement('div');
-            cardElement.classList.add('card');
-            cardElement.style.backgroundImage = `url(cards/${value}${suit}.jpg)`;
-            deckElement.appendChild(cardElement);
-        });
-    });
+var dealerAce = 0;
+var playerAce = 0;
+
+var hidden;
+var deck;
+
+var canHit = true; //allows player to draw while sum is lower than 21
+
+window.onload = function () {
+    buildDeck();
+    shuffleDeck();
+    startGame();
 }
-window.onload = createCards;
+
+function buildDeck() {
+    let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let types = ["C", "D", "H", "S"];
+    deck = [];
+
+    for (let i = 0; i < types.length; i++) {
+        for (let j = 0; j < values.length; j++) {
+            deck.push(values[j] + types[i]);
+        }
+    }
+    console.log(deck);
+}
+
+function shuffleDeck() {
+    for (let i = 0; i < deck.length; i++) {
+        let j = Math.floor(Math.random() * deck.length);
+        let temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+    }
+    console.log(deck);
+}
+
+function startGame(){
+    hidden = deck.pop();
+    dealerSum += getValue(hidden);
+    dealerAce += checkAce(hidden);
+    while (dealerSum < 17){
+        let cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src = "./cards/" + card + ".jpg";
+        dealerSum += getValue(card);
+        dealerAce += checkAce(card);
+        document.getElementById("dealer_cards").append(cardImg);
+    }
+    console.log(dealerSum);
+}
+
+function getValue(card){
+    let data = card;
+    let value = data[0];
+
+    if(isNaN(value)){
+        if (value == "A"){
+            return 11;
+        }
+        return 10;
+    }
+    return parseInt(value);
+}
+
+function checkAce(card){
+    if(card[0] == "A"){
+        return 1;
+    }
+}
